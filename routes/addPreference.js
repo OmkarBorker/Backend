9ddeb48df38fae03c66998721f8e897d49ba0a84
VishalKharde123 <Vishal.Kharde@winjit.com>
2024-07-23 16:43:05 +0530

    Updated addprefereces api

--- routes/addPreference.js
+++ routes/addPreference.js
@@ -3,12 +3,24 @@ const UserModel = require('../model/UserModel');
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-    await UserModel.findOneAndUpdate(
-        { _id: req.body.userID }, // Filter
-        { $set: { preferences: req.body.preferences } }, // Update
-        { new: true } // Options: return the updated document
-    );
-    res.send('API')
+    try {
+        await UserModel.findOneAndUpdate(
+            { _id: req.body.userID }, // Filter
+            { $set: { preferences: req.body.preferences } }, // Update
+            { new: true } // Options: return the updated document
+        );
+        return res.status(200).json({
+            status: "Success",
+            message: "Preferences added",
+            token: token
+        })
+    }
+    catch (e) {
+        return res.status(500).json({
+            status: "failed",
+            message: e.message
+        })
+    }
 });
 
 module.exports = router;
